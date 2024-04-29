154856f20fc52ed9fce35b519074e1db1e60a04f
ImogenWren <imogen.wren91@gmail.com>
2024-04-29 13:24:17 +0100

    dfhnerthn

--- firmware/ripple-tank-test-firmware/ripple-tank-test-firmware.ino
+++ firmware/ripple-tank-test-firmware/ripple-tank-test-firmware.ino
@@ -13,26 +13,57 @@
 autoDelay printDelay;
 #define PRINT_DELAY 1000
 autoDelay strobeDelay;
+autoDelay rippleDelay;
+
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\nRipple Tank - Prototype\n");
   analogWrite(LED_PWM_PIN, 0);
+  pinMode(LED_PWM_PIN, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
 }
 
 
 bool strobe_state = true;
 
+bool wave_state = false;
+
+void run_LFO(uint32_t period) {
+  if (rippleDelay.microsDelay(period)) {
+    if (wave_state) {
+      wave_state = false;
+    } else {
+      wave_state = true;
+    }
+    digitalWrite(SPEAKER_PIN, wave_state);
+  }
+}
 
 void loop() {
   uint16_t brightness_val, strobe_val, frequency_val;
 
   brightness_val = map(analogRead(BRIGHTNESS_PIN), 0, 1024, 0, 255);
   strobe_val = analogRead(STROBE_PIN);
-  frequency_val = map(analogRead(FREQUENCY_PIN), 0, 1024, 32, 2000);
-  tone(SPEAKER_PIN, frequency_val);
+  frequency_val = map(analogRead(FREQUENCY_PIN), 0, 1024, 0, 204);
+  // frequency_val = analogRead(FREQUENCY_PIN);
 
+  // frequency = frequency * 0.1
+  float Hz = float(frequency_val * frequency_val * 0.01);
+  //  if (frequency_val < 32) {
+
+  // } else {
+  //   tone(SPEAKER_PIN, frequency_val);
+  //  }
+
+  uint32_t period_uS = uint32_t((1000000 / Hz) + 1);
+
+  if (period_uS) {
+    run_LFO(period_uS);
+  } else {
+    digitalWrite(SPEAKER_PIN, false);
+  }
 
   if (strobe_val < 2) {
     analogWrite(LED_PWM_PIN, brightness_val);
@@ -50,7 +81,11 @@ void loop() {
   }
 
   char buffer[64];
-  sprintf(buffer, "brightness: %4u, strobe: %4u, frequency: %4u", brightness_val, strobe_val, frequency_val);
+  char HzBuffer[16];
+  char periodBuffer[16];
+  dtostrf(Hz, 4, 2, HzBuffer);
+  // dtostrf(period_uS, 4, 2, periodBuffer);
+  sprintf(buffer, "brightness: %4u, strobe: %4u, frequency: %4s Hz %4u uS", brightness_val, strobe_val, HzBuffer, period_uS);
 
   if (printDelay.millisDelay(PRINT_DELAY)) {
     Serial.println(buffer);
